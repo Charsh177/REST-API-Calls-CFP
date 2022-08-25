e2ba70b54f22d0f4c88b543584ba08af36d9867d
Charsh177 <harshchougule177@gmail.com>
2022-08-25 12:04:45 +0530

    ADDED: Rest Controller to demonstrate HTTP methods ade response msg to user

---
+++ src/main/java/com/bridgelabz/restapi/RestApiCallApplication.java
@@ -0,0 +1,15 @@
+package com.bridgelabz.restapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RestApiCallApplication {
+
+	public static void main(String[] args) {
+		
+		System.out.println("Hello from BridgeLabz");
+		SpringApplication.run(RestApiCallApplication.class, args);
+	}
+
+}
