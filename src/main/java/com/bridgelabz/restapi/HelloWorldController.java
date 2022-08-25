bfffdd907cc6420bbd840ac3c345f01662096fe2
Charsh177 <harshchougule177@gmail.com>
2022-08-25 13:02:22 +0530

    ADDED: Create spring app to demonstarte http methods and display message

--- src/main/java/com/bridgelabz/restapi/HelloWorldController.java
+++ src/main/java/com/bridgelabz/restapi/HelloWorldController.java
@@ -2,8 +2,6 @@ package com.bridgelabz.restapi;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -19,11 +17,5 @@ public class HelloWorldController {
 	@GetMapping("/get1")
 	public String hello1() {
 		return "Hello BridgeLabz";
-	}
-	
-	// Use Case 2
-	@RequestMapping(value = {"/query"}, method = RequestMethod.GET)
-	public String sayHello(@RequestParam(value = "name") String name) {
-		return "Hello " + name + "!";
 	}
 }
